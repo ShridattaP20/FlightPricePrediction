22049d46756f389a89598b7796e2ed4fc6b0d5e0
Shridatta <shridatta.patil@gmail.com>
2022-09-05 12:52:44 +0530

    Web app is ready

---
+++ app.py
@@ -0,0 +1,41 @@
+import pickle
+import os
+from flask import Flask, request, app, jsonify, render_template, url_for
+import pandas as pd
+import numpy as np
+
+app=Flask(__name__)
+
+ ## Load the model
+reg_model=pickle.load(open('xgb_reg_model.pkl','rb'))
+scaler=pickle.load(open('scaling.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    data=request.json['data']
+    print(data)
+    print(np.array(list(data.values())).reshape(1,-1))
+    new_data=scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    output=reg_model.predict(new_data)
+    print(output[0])
+    return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_data=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_data)
+    output=reg_model.predict(final_data)[0]
+    return render_template('home.html',prediction_text="The Flight Price Prediction is {}".format(output))
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
+
+
+    
